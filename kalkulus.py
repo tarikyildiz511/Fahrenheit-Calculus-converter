6afbb92d97ace8a79286b439ce2b7f2b5b6e922a
Tarık <70340756+tarikyildiz511@users.noreply.github.com>
2020-12-13 01:37:32 +0300

    Add files via upload

---
+++ kalkulus.py
@@ -0,0 +1,32 @@
+print("-"*30)
+print("1- kalkülüs")
+print("2-fahrenheit")
+
+seçim = int(input("1/2?"))
+if seçim ==1:
+    print("\n celsius to  fahrenheit") 
+    celsius = float(input("Derece girin"))
+    fahrenheit = (celsius*1.8)+32
+    print(f"{celsius}kalkulus{fahrenheit}")
+elif seçim ==2:
+    print("\n fahrenheit to celsius") 
+    fahrenheit = float(input("Derece girin"))
+    celsius = (fahrenheit-32) /1.8
+    print(f"{fahrenheit}, to {celsius}")
+
+print("-"*30)
+print("1-celsius")
+print("2-fahrenheit")
+secim = int(input("1/2"))
+
+
+if secim == 1:
+    print("\n celsius to fahrenheit")
+    celsius = float(input("derece giriniz"))
+    fahrenheit = (celsius*1.8) +32
+    print(f"{celsius}celsius {fahrenheit}fahrenheit")
+elif secim ==2:
+    print("\n celsius to fahrenheit")
+    fahrenheit = float(input("derece giriniz"))
+    celsius = (fahrenheit-32) / 1.8
+    print(f"{fahrenheit}fahrenheit{celsius}celsius")
